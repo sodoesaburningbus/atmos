57a0980cd89c06d215213f2f06a07901e54acb71
Christopher E. Phillips <cephillips574@gmail.com>
2023-03-30 21:39:48 -0500

    Corrected for atmos_math name change

--- thermo.py
+++ thermo.py
@@ -29,7 +29,7 @@
 # ~Christopher Phillips
 
 #Import required modules
-import atmos.math as am
+import atmos.atmos_math as am
 import numpy
 
 ############################################################################
